1937522906401c20dda0858e37ef38561e572a11
AlessandroCampo <alessandrocampo97@gmail.com>
2023-12-22 18:24:41 +0100

    trying to add timetables for trains

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const milan = {
     latitude: 45.4654219,
-    longitude: 9.1859243
+    longitude: 9.1859243,
+    departure_times: ["06:12", "09:38", "12:15", "18:03"]
 }
 
-const turin = {
-    latitude: 45.070312,
-    longitude: 7.6868565
+const bologna = {
+    latitude: 44.494887,
+    longitude: 11.3426163,
+
 }
 
 const rome = {
@@ -38,11 +40,25 @@ let arrivalCoords = {
     longitude: 0
 }
 
-let travelDistance = 0;
+
+const price_multiplier = 0.21;
 let button = document.getElementById("button")
 let starting_station = document.getElementById("starting-station")
 let arrival_station = document.getElementById("arriving-station")
+let dep_time = document.getElementById("departure-time")
 let distance_info = document.getElementById("distance_travelled")
+let passengers_info = document.getElementById("passengers_number")
+let hours_info = document.getElementById("hours")
+let price_info = document.getElementById("ticket_price")
+let plus_button = document.getElementById("plus")
+let minus_button = document.getElementById("minus")
+let plus_elder_button = document.getElementById("elder_plus")
+let minus_elder_button = document.getElementById("elder_minus")
+let passenger_number = 1;
+let elder_number = 0;
+let passengers_number_selection = document.getElementById("passenger_number")
+let elders_number_selection = document.getElementById("elder_number")
+let availableTimes = []
 
 function degreesToRadians(degrees) {
     var radians = (degrees * Math.PI) / 180;
@@ -50,10 +66,46 @@ function degreesToRadians(degrees) {
 }
 
 
+plus_button.addEventListener("click", () => {
+    if (passenger_number + elder_number < 10) {
+        passenger_number++;
+        passengers_number_selection.value = passenger_number
+    }
 
+    else {
+        alert("il numero massimo di biglietti acquistabilli è 10")
+    }
+
+})
+
+minus_button.addEventListener("click", () => {
+    if (passenger_number > 0) {
+        passenger_number--;
+        passengers_number_selection.value = passenger_number
+    }
+})
+
+plus_elder_button.addEventListener("click", () => {
+    if (passenger_number + elder_number < 10) {
+        elder_number++;
+        elders_number_selection.value = elder_number
+    }
+
+    else {
+        alert("il numero massimo di biglietti acquistabilli è 10")
+    }
+
+})
+
+minus_elder_button.addEventListener("click", () => {
+    if (elder_number > 0) {
+        elder_number--;
+        elders_number_selection.value = elder_number
+    }
+})
 
 button.addEventListener("click", () => {
-
+    availableTimes = []
 
     if (starting_station.value == "Napoli") {
         startCoords.latitude = naples.latitude
@@ -61,8 +113,8 @@ button.addEventListener("click", () => {
     }
 
     else if (starting_station.value == "Torino") {
-        startCoords.latitude = turin.latitude
-        startCoords.longitude = turin.longitude
+        startCoords.latitude = bologna.latitude
+        startCoords.longitude = bologna.longitude
     }
 
     else if (starting_station.value == "Roma") {
@@ -73,6 +125,14 @@ button.addEventListener("click", () => {
     else if (starting_station.value == "Milano") {
         startCoords.latitude = milan.latitude
         startCoords.longitude = milan.longitude
+        console.log(dep_time.value)
+        milan.departure_times.forEach((time) => {
+            if (time > dep_time.value) {
+                availableTimes.push(time)
+                console.log(availableTimes)
+            }
+
+        })
     }
 
     else if (starting_station.value == "Villa S.Giovanni") {
@@ -86,8 +146,8 @@ button.addEventListener("click", () => {
     }
 
     else if (arrival_station.value == "Torino") {
-        arrivalCoords.latitude = turin.latitude
-        arrivalCoords.longitude = turin.longitude
+        arrivalCoords.latitude = bologna.latitude
+        arrivalCoords.longitude = bologna.longitude
     }
     else if (arrival_station.value == "Roma") {
         arrivalCoords.latitude = rome.latitude
@@ -104,6 +164,11 @@ button.addEventListener("click", () => {
         arrivalCoords.longitude = vsg.longitude
     }
 
+    if (arrival_station.value == starting_station.value) {
+        alert("le stazioni di partenza e di arrivo non possono coincidere")
+    }
+
+    milan.departure_times.forEach
     console.log(startCoords.latitude)
     calcDistance()
 
@@ -127,6 +192,9 @@ function calcDistance() {
         Math.cos(startingLong - destinationLong)) * radius;
 
     let myDistance = Math.floor(distanceInKilometers)
-    distance_info.innerText = myDistance + "km"
+    distance_info.innerText = myDistance + " km"
+    passengers_info.innerText = passenger_number + " passeggeri adulti " + elder_number + " passeggeri anziani"
+    price_info.innerText = ((myDistance * price_multiplier) * (passenger_number + (elder_number * 4 / 10))).toFixed(2) + " euro"
+    hours_info.innerText = availableTimes
 
 }
