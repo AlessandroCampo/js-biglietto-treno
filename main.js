3a0ec5daedcbc86fbaccd5b528a5191f4c516f43
AlessandroCampo <alessandrocampo97@gmail.com>
2023-12-23 14:59:38 +0100

    validation about invalid hour\date, table generation with timetable options

--- main.js
+++ main.js
@@ -66,9 +66,12 @@ let todayDate = currentDate.getDate()
 let todayMonth = currentDate.getMonth()
 let todayYear = currentDate.getFullYear()
 let currentFullDate = `${todayYear}-${todayMonth + 1}-${todayDate}`;
-let currentFullDateNextYear = `${todayYear + 1}-${todayMonth + 1}-${todayDate}`;
+let currentFullDateNextYear = `${todayYear + 1}-${todayMonth + 1}-${todayDate}`
+let dep_hours = ["8:00", "9:17", "11:05", "13:30", "15:12", "17:00", "18:45", "19:30", "20:55", "22:04"]
 dep_day.setAttribute("min", currentFullDate)
 dep_day.setAttribute("max", currentFullDateNextYear)
+
+
 
 
 function degreesToRadians(degrees) {
@@ -77,14 +80,17 @@ function degreesToRadians(degrees) {
 }
 
 
+
+
+
 plus_button.addEventListener("click", () => {
-    if (passenger_number + elder_number < 10) {
+    if (passenger_number + elder_number < 5) {
         passenger_number++;
         passengers_number_selection.value = passenger_number
     }
 
     else {
-        alert("il numero massimo di biglietti acquistabilli è 10")
+        alert("il numero massimo di biglietti acquistabilli in una singola sessione è 5")
     }
 
 })
@@ -116,13 +122,14 @@ minus_elder_button.addEventListener("click", () => {
 })
 
 button.addEventListener("click", () => {
+    let time_distance = "0:00"
 
     if (starting_station.value == "Napoli") {
         startCoords.latitude = naples.latitude
         startCoords.longitude = naples.longitude
     }
 
-    else if (starting_station.value == "Torino") {
+    else if (starting_station.value == "Bologna") {
         startCoords.latitude = bologna.latitude
         startCoords.longitude = bologna.longitude
     }
@@ -147,7 +154,7 @@ button.addEventListener("click", () => {
         arrivalCoords.longitude = naples.longitude
     }
 
-    else if (arrival_station.value == "Torino") {
+    else if (arrival_station.value == "Bologna") {
         arrivalCoords.latitude = bologna.latitude
         arrivalCoords.longitude = bologna.longitude
     }
@@ -166,64 +173,158 @@ button.addEventListener("click", () => {
         arrivalCoords.longitude = vsg.longitude
     }
 
-    if (arrival_station.value == starting_station.value) {
-        alert("le stazioni di partenza e di arrivo non possono coincidere")
-    }
+
 
     if ((starting_station.value == "Milano") && (arrival_station.value == "Bologna") ||
         (starting_station.value == "Bologna") && (arrival_station.value == "Milano")) {
-        console.log("bologna-milano")
+        time_distance = "1:15"
     }
 
     if ((starting_station.value == "Milano") && (arrival_station.value == "Roma") ||
         (starting_station.value == "Roma") && (arrival_station.value == "Milano")) {
-        console.log("roma-milano")
+        time_distance = "3:40"
     }
 
     if ((starting_station.value == "Milano") && (arrival_station.value == "Napoli") ||
         (starting_station.value == "Napoli") && (arrival_station.value == "Milano")) {
-        console.log("napoli-milano")
+        time_distance = "5:15"
     }
 
     if ((starting_station.value == "Milano") && (arrival_station.value == "Villa S.Giovanni") ||
         (starting_station.value == "Villa S.Giovanni") && (arrival_station.value == "Milano")) {
-        console.log("villa-milano")
+        time_distance = "8:45"
     }
 
     if ((starting_station.value == "Bologna") && (arrival_station.value == "Roma") ||
         (starting_station.value == "Roma") && (arrival_station.value == "Bologna")) {
-        console.log("bolo-roma")
+        time_distance = "2:25"
     }
 
     if ((starting_station.value == "Bologna") && (arrival_station.value == "Napoli") ||
         (starting_station.value == "Napoli") && (arrival_station.value == "Bologna")) {
-        console.log("bolo-napoli")
+        time_distance = "3:45"
     }
 
     if ((starting_station.value == "Bologna") && (arrival_station.value == "Villa S.Giovanni") ||
         (starting_station.value == "Villa S.Giovanni") && (arrival_station.value == "Bologna")) {
-        console.log("bolo-villa")
+        time_distance = "7:30"
     }
 
     if ((starting_station.value == "Roma") && (arrival_station.value == "Napoli") ||
         (starting_station.value == "Napoli") && (arrival_station.value == "Roma")) {
-        console.log("roma-napoli")
+        time_distance = "1:15"
     }
 
     if ((starting_station.value == "Roma") && (arrival_station.value == "Villa S.Giovanni") ||
         (starting_station.value == "Villa S.Giovanni") && (arrival_station.value == "Roma")) {
-        console.log("roma-vila")
+        time_distance = "6:05"
     }
 
     if ((starting_station.value == "Napoli") && (arrival_station.value == "Villa S.Giovanni") ||
         (starting_station.value == "Villa S.Giovanni") && (arrival_station.value == "Napoli")) {
-        console.log("villa-napoli")
+        time_distance = "4:03"
     }
 
 
 
-    calcDistance()
-    hours_info.innerText = dep_time.value
+
+    if (arrival_station.value == starting_station.value) {
+        alert("le stazioni di partenza e di arrivo non possono coincidere")
+    }
+
+    else if (currentDate.getHours() > dep_time.value.split(":")[0] && currentFullDate == dep_day.value) {
+        alert("attenzione, a meno che tu non abbia una macchina del tempo non puoi prendere questo treno!")
+    }
+
+    else if (dep_day.value == "") {
+        alert("Si prega di inserire una data di partenza")
+    }
+
+
+
+    else {
+        let trs = document.querySelectorAll(".train")
+        let table_body = document.getElementById("table_body")
+        let trains_table = document.getElementById("trains")
+        trains_table.classList.remove("d-none")
+        trs.forEach((tr) => table_body.removeChild(tr))
+
+        calcDistance()
+        dep_hours.forEach((hour) => {
+
+            let final_time
+            function travelTimeCalc() {
+                let minutes1 = Number(hour.split(":")[1])
+                let hours1 = Number(hour.split(":")[0])
+                let minutes2 = Number(time_distance.split(":")[1])
+                let hours2 = Number(time_distance.split(":")[0])
+                let result_hour = (hours1 + hours2)
+                let result_minutes = (minutes1 + minutes2)
+                if (result_minutes >= 60) {
+                    result_minutes = result_minutes - 60
+                    result_hour++;
+                }
+
+                if (result_hour >= 24) {
+                    result_hour = result_hour - 24;
+                }
+
+
+                result_hour = result_hour.toLocaleString("en-US", {
+                    minimumIntegerDigits: 2
+                })
+
+                result_minutes = result_minutes.toLocaleString("en-US", {
+                    minimumIntegerDigits: 2
+                })
+
+                final_time = `${result_hour}:${result_minutes}`
+
+            }
+
+            travelTimeCalc()
+
+
+
+            let start_arrive = document.createElement("th")
+            let time_dep_arr = document.createElement("th")
+            let date = document.createElement("th")
+            let duration = document.createElement("th")
+            let total_price = document.createElement("th")
+            let select = document.createElement("th")
+            let train = document.createElement("tr")
+            let select_button = document.createElement("button")
+
+
+            train.className = "train"
+
+            if (Number(dep_time.value.split(":")[0]) < Number(hour.split(":")[0])) {
+                start_arrive.innerText = `${starting_station.value} - ${arrival_station.value}`
+                time_dep_arr.innerText = `${hour} - ${final_time}`
+                duration.innerText = time_distance
+                date.innerText = dep_day.value
+                total_price.innerText = price_info.innerText
+                select_button.classList.add("btn.btn-primary")
+                select_button.innerText = "Seleziona"
+
+
+
+                table_body.appendChild(train)
+                train.appendChild(start_arrive)
+                train.appendChild(time_dep_arr)
+                train.appendChild(duration)
+                train.appendChild(date)
+                train.appendChild(total_price)
+                select.appendChild(select_button)
+                train.appendChild(select)
+            }
+
+        })
+
+    }
+
+
+
 
 
 
